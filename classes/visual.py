7863a583f98a31c84c4d6e7b9f890905cc25f63f
Amande <31880536+amandinecaut@users.noreply.github.com>
2025-11-03 16:51:53 +0100

    Update visual.py

--- classes/visual.py
+++ classes/visual.py
@@ -252,7 +252,7 @@ class DistributionPlot:
                 x=1,
                 font=dict(size=14)
             ),
-            margin=dict(l=90, r=400, t=40, b=40),  
+            margin=dict(l=90, r=410, t=40, b=40),  
             autosize=False,
             height=400 + 5* len(self.cols),  # scales height with number of violins
             width=800
