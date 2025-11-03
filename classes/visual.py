869caa7a81a6ef55477bb53340abe157e15fb0c2
Amande <31880536+amandinecaut@users.noreply.github.com>
2025-11-03 16:50:15 +0100

    Adjust right margin in visual.py

--- classes/visual.py
+++ classes/visual.py
@@ -252,7 +252,7 @@ class DistributionPlot:
                 x=1,
                 font=dict(size=14)
             ),
-            margin=dict(l=90, r=350, t=40, b=40),  
+            margin=dict(l=90, r=300, t=40, b=40),  
             autosize=False,
             height=400 + 5* len(self.cols),  # scales height with number of violins
             width=800
