bfe08b9b36466cc0a38f880d4e880a06a717b2aa
Amande <31880536+amandinecaut@users.noreply.github.com>
2025-11-03 16:48:13 +0100

    Adjust column widths in app layout

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def show_entity_plots(entity_type, entity_name, metrics):
         
     
 
-    center_col = st.columns([0.5, 9, 0.5])[1]
+    center_col = st.columns([0.2, 10, 0.2])[1]
     with center_col:
         visual_distribution.show()
 
